11adf71fdd176ebd61532025fe9e49a90668cdcb
Daniel Bocharnikov <dbocharnikov92@gmail.com>
2019-04-03 00:13:33 +0300

    Updates

---
+++ precache-manifest.16444dfe86819954aacdef0c364c9a5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "fa09af9faacd306ba041",
+    "url": "/contactmanager/static/js/main.376856cd.chunk.js"
+  },
+  {
+    "revision": "586b0063f22caf176484",
+    "url": "/contactmanager/static/js/2.52392625.chunk.js"
+  },
+  {
+    "revision": "fa09af9faacd306ba041",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "586b0063f22caf176484",
+    "url": "/contactmanager/static/css/2.e5475b05.chunk.css"
+  },
+  {
+    "revision": "87bac69d941fa6a102dfabb6a4ab5dad",
+    "url": "/contactmanager/index.html"
+  }
+];
